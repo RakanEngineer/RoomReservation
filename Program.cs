af68f8cc16b96e301eeb972b861859d0b979da83
RakanEngineer <rakan.engineer@gmail.com>
2024-09-23 10:57:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace RoomReservation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isValidUser = false;
+            while (!isValidUser)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(2, 2);
+                Console.Write("Username: ");
+                Console.SetCursorPosition(2, 4);
+                Console.Write("Password: ");
+                Console.SetCursorPosition(12, 2);
+                string username = Console.ReadLine();
+                Console.SetCursorPosition(12, 4);
+                string password = Console.ReadLine();
+                Console.Clear();
+                bool accessGranted = username == "admin" && password == "secret";
+                if (accessGranted)
+                {
+                    isValidUser = true;
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                    Thread.Sleep(2000); // 2000 ms = 2 sek
+                }
+            }
+        }
+    }
+}
